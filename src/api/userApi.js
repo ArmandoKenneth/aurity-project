619934a38c316f74c8ee4a1ca6b458ca4ba71226
Armando Rocha <armando.kenneth@gmail.com>
2017-05-27 17:30:02 -0300

    Add redux and other dependencies

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,16 @@
+/*
+Keeping all user related api calls to this file
+*/
 class UserApi {
 	
 	static getAllUsers() {
-		
+		return fetch('https://timesheet-staging-aurity.herokuapp.com/api/users').then(response => {
+			return response.json();
+		}).catch(error => {
+			return error;
+		});	
 	}
 
 }
+
+export default UserApi;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+import { Router, browserHistory } from 'react-router';  
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)	
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
